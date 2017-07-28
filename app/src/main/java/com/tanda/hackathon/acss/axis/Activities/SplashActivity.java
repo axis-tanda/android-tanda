12a1f9f306e67ecf1a0d47f5e9f3b9bc031e6588
keanpaderes <kean.paderes@gmail.com>
2017-07-28 22:51:18 +0800

    Clock in beacon monitoring added.

--- app/src/main/java/com/tanda/hackathon/acss/axis/Activities/SplashActivity.java
+++ app/src/main/java/com/tanda/hackathon/acss/axis/Activities/SplashActivity.java
@@ -1,7 +1,9 @@
-package com.tanda.hackathon.acss.axis;
+package com.tanda.hackathon.acss.axis.Activities;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import com.estimote.sdk.SystemRequirementsChecker;
+import com.tanda.hackathon.acss.axis.R;
 
 public class SplashActivity extends AppCompatActivity {
 
@@ -9,5 +11,12 @@ public class SplashActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        SystemRequirementsChecker.checkWithDefaultDialogs(this);
     }
 }
